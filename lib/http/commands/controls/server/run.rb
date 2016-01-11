fa3361d6b4b12d1fa9e84ff780d47d694122f359
ntl <nathanladd+github@gmail.com>
2016-01-11 11:42:36 -0600

    Remove unnecessary ExecutionContext abstraction

--- lib/http/commands/controls/server/run.rb
+++ lib/http/commands/controls/server/run.rb
@@ -2,11 +2,25 @@ module HTTP
   module Commands
     module Controls
       class Server
-        module Run
-          def self.call(server, port: nil, &block)
+        class Run
+          attr_reader :block
+          attr_reader :port
+          attr_accessor :scheduler
+
+          def initialize(block, port)
+            @block = block
+            @port = port
+          end
+
+          def self.build(port=nil, &block)
             port ||= 8000
 
-            client = ExecutionContext.build port, &block
+            instance = new block, port
+            instance
+          end
+
+          def self.call(server, port: nil, &block)
+            client = build port, &block
 
             cooperation = ProcessHost::Cooperation.build
             cooperation.register server, 'server'
@@ -14,29 +28,13 @@ module HTTP
             cooperation.start
           end
 
-          class ExecutionContext
-            attr_reader :block
-            attr_reader :port
-            attr_accessor :scheduler
+          def start
+            block.(port, scheduler)
+          end
 
-            def initialize(block, port)
-              @block = block
-              @port = port
-            end
-
-            def self.build(port, &block)
-              instance = new block, port
-              instance
-            end
-
-            def start
-              block.(port, scheduler)
-            end
-
-            module ProcessHostIntegration
-              def change_connection_scheduler(scheduler)
-                self.scheduler = scheduler
-              end
+          module ProcessHostIntegration
+            def change_connection_scheduler(scheduler)
+              self.scheduler = scheduler
             end
           end
         end
