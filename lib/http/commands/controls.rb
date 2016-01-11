12254a8aadf29da805c38c0ca209317cc44617a4
ntl <nathanladd+github@gmail.com>
2016-01-11 11:24:17 -0600

    Stop required antiquated controls

--- lib/http/commands/controls.rb
+++ lib/http/commands/controls.rb
@@ -3,13 +3,8 @@ require 'process_host'
 
 require 'connection/controls'
 
-require 'http/commands/controls/connections'
 require 'http/commands/controls/dialogs'
 require 'http/commands/controls/messages'
 require 'http/commands/controls/run_server'
 require 'http/commands/controls/server'
-require 'http/commands/controls/substitute_connection'
 require 'http/commands/controls/uri'
-
-require 'http/commands/controls/data'
-require 'http/commands/controls/connections'
