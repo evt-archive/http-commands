94ad12565d32ab093d59704954213e93ab5b1cdb
Scott Bellware <sbellware@gmail.com>
2016-03-30 02:11:19 -0500

    Connection normalization doesn't need to be instance methods

--- lib/http/commands/action.rb
+++ lib/http/commands/action.rb
@@ -5,6 +5,8 @@ module HTTP
         cls.extend Actuate
         cls.extend Build
         cls.extend Configure
+        cls.extend Connection
+        cls.extend Logger
 
         cls.class_exec do
           # TODO Is this otherwise a dependency? [Scott, Wed Mar 30 2016]
@@ -22,9 +24,6 @@ module HTTP
       end
 
       module Build
-        # TODO Why is this an optional positional param when the other methods at this level are named param? [Scott, Wed Mar 30 2016]
-        # Postulation: because it's the only param
-        # This pattern is in other libraries as well
         def build(connection=nil, &blk)
           new.tap do |instance|
             Telemetry::Logger.configure instance
@@ -35,11 +34,43 @@ module HTTP
 
       module Configure
         def configure(receiver, attr_name=nil, connection: nil)
+          # TODO Default attr_name cannot be get for any command other than get
           attr_name ||= :get
 
           instance = build connection
           receiver.send "#{attr_name}=", instance
           instance
+        end
+      end
+
+      module Connection
+        def get_connection(connection, uri, headers, &blk)
+          if connection.nil?
+            connection = get_one_time_connection(uri, headers, &blk)
+          end
+
+          connection
+        end
+
+        def get_one_time_connection(uri, headers, &blk)
+          logger.trace "Creating one-time connection"
+
+          headers['Connection'] ||= 'close'
+          connection = Connect.(uri)
+
+          if blk
+            blk.(connection)
+          end
+
+          logger.debug "Created one-time connection"
+
+          connection
+        end
+      end
+
+      module Logger
+        def logger
+          Telemetry::Logger.get self
         end
       end
 
@@ -50,7 +81,7 @@ module HTTP
         # TODO uri is a candidate for instance member (it's passed around) [Scott, Wed Mar 30 2016]
         uri = URI(uri)
 
-        connection = get_connection(self.connection, uri, headers, &blk)
+        connection = self.class.get_connection(self.connection, uri, headers, &blk)
 
         Request.(
           connection,
@@ -60,29 +91,6 @@ module HTTP
           body: body,
           headers: headers
         )
-      end
-
-      def get_connection(connection, uri, headers, &blk)
-        if connection.nil?
-          connection = get_one_time_connection(uri, headers, &blk)
-        end
-
-        connection
-      end
-
-      def get_one_time_connection(uri, headers, &blk)
-        logger.trace "Creating one-time connection"
-
-        headers['Connection'] ||= 'close'
-        connection = Connect.(uri)
-
-        if blk
-          blk.(connection)
-        end
-
-        logger.debug "Created one-time connection"
-
-        connection
       end
     end
   end
